181ef6ebc6ee2f00375a8baa044de81a90976868
Yar177 <yarsher.dev@gmail.com>
2019-09-17 16:55:30 -0400

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Hoshiar Sher on 9/17/19.
+//  Copyright © 2019 AlepTau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
